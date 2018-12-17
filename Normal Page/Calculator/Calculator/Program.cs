560abc5e042d55393cf4ebae35241ad3ab441c09
Kreimben <99jhkim@naver.com>
2018-12-17 21:56:58 +0900

    Add files via upload

---
+++ Normal Page/Calculator/Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("첫 번째 숫자를 입력하세요.");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("두 번째 숫자를 입력하세요.");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("연산자를 입력하세요.");
+            string inputOperator = Console.ReadLine();
+
+            Console.Write(num1);
+            Console.Write(inputOperator);
+            Console.Write(num2);
+            Console.Write(" = ");
+            
+            if (inputOperator == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else
+            {
+                if (inputOperator == "-")
+                {
+                    Console.WriteLine(num1 - num2);
+                }
+
+                if (inputOperator == "*")
+                {
+                    Console.WriteLine(num1 * num2);
+                }
+
+                if (inputOperator == "/")
+                {
+                    Console.WriteLine(num1 / num2);
+                }
+            }
+        }
+    }
+}
---
+++ Normal Page/ComparisonOperators/ComparisonOperators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("10 > 10 은 ");
+            Console.WriteLine(10 > 10);  // False!!!
+
+            Console.Write("10 >= 10 은 ");
+            Console.WriteLine(10 >= 10);  // True!!!
+
+            Console.Write("5 <= 10 은 ");
+            Console.WriteLine(5 <= 10);  // True!!!
+
+            Console.Write("5 == 4 는 ");
+            Console.WriteLine(5 == 4);  // False!!!
+
+            Console.Write("5 != 4 는 ");
+            Console.WriteLine(5 != 4);  // True!!!
+        }
+    }
+}
