af2a3ded61b6fc7377ef068ed234c265ef18cb82
Kreimben <99jhkim@naver.com>
2018-12-17 21:53:53 +0900

    Add files via upload

---
+++ Normal Page/WrongVariable/WrongVariable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WrongVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int appleCount = 12;
+            10 = appleCount;        // = 왼쪽에는 변수만 올 수 있어서 오류!
+
+
+
+            int pearCount;
+            Console.WriteLine(appleCount);
+            Console.WriteLine(pearCount); // 변수에 값을 넣지 않고사용해서 오류!!
+        }
+    }
+}
