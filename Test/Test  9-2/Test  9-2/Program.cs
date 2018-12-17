c401188ee8c3ee75e4921bf1b37590fc713727d4
Kreimben <99jhkim@naver.com>
2018-12-17 22:15:54 +0900

    Add files via upload

---
+++ Test/Test  9-2/Test  9-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test__9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            int totalScoreSum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(i + 1);
+                Console.WriteLine("번째 과목의 성적을 입력하세요.");
+                scores[i] = int.Parse(Console.ReadLine());
+                totalScoreSum += scores[i];
+            }
+
+            Console.Write("총점은 ");
+            Console.Write("\"" + totalScoreSum + "\"");
+            Console.WriteLine("점입니다.");
+        }
+    }
+}
