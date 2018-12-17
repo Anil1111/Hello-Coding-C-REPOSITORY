d793d722de44bf83ab50ce0c5c460e58db591418
Kreimben <99jhkim@naver.com>
2018-12-17 22:10:35 +0900

    Add files via upload

---
+++ Normal Page/Operators/Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("100 + 10 = ");
+            Console.WriteLine(100+10);
+            Console.Write("100 - 10 = ");
+            Console.WriteLine(100-10);
+            Console.Write("100 * 10 = ");
+            Console.WriteLine(100 * 10);
+            Console.Write("84 / 10 = ");
+            Console.WriteLine(84 /10);
+            Console.Write("84 % 10 = ");
+            Console.WriteLine(84 % 10);
+
+            Console.WriteLine("글" + "붙여쓰기");
+            Console.WriteLine("글붙여쓰기" + "붙여쓰기"); // 문자열에는 '-'연산자가 없어서 오류!
+        }
+    }
+}
